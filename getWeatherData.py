07ca3625b4b7088f7889d648232ec23d337aa022
Daniel Carmona <djcarmona@icloud.com>
2019-05-26 15:08:02 -0500

    updated get weather app to include more days of forecast

--- getWeatherData.py
+++ getWeatherData.py
@@ -4,11 +4,10 @@ import re
 import csv
 import sys
 cnt = 1 
-header = "elevation[meters], WeatherDataValidUntil, temperature[°F], temperatureTrend, windDirection, windSpeed, shortForecast\n" 
+header = "elevation[meters], windDirection, temperature[°F], isDaytime, detailedForecast, shortForecast, windSpeed, startTime, number, endTime, name, temperatureTrend, " 
 outString = ''
-parameters = csv.reader(open('mainData.csv', 'r')) #get long and lat parameters from mainData file
+parameters = csv.reader(open('mainData.csv', 'r')) #get long and lat parameters from mainData filewindDir
 parameter = next(parameters)[12] #row=0, col=12, note this is the header row
-
 
 while(cnt<=314):
     try:
@@ -22,28 +21,39 @@ while(cnt<=314):
         print(longitude + ', ' + latitude)
         weatherData = n.points_forecast(longitude, latitude, hourly=False) #gets data string
         data = weatherData.pop('properties') #access the key of interest from dict object
-        todayWeather = data.pop('periods') #creates list from keys
+        todayWeather = data.pop('periods') #creates list from keys 
         elevationData = data.pop('elevation')
         elevation = elevationData.get('value')
         print(elevation)
+        outString += str(elevation) + ', '
         #unit = elevationData.get('unitCode') #formats unit string for elevation 
         #elevation = str(elevation) #+ str(unit) #ommited since all units are meter and easy to express
         #elevation = elevation.replace('unit:', '')
-        temp = todayWeather[0].get('temperature')
-        time = todayWeather[0].get('endTime')
-        tempTrend = todayWeather[0].get('temperatureTrend')
-        windDir = todayWeather[0].get('windDirection')
-        windSpeed = todayWeather[0].get('windSpeed')
-        shortForcast = todayWeather[0].get('shortForecast') 
-        outString += str(elevation) + ', ' + str(time) + ', ' + str(temp) + ', ' + str(tempTrend) + ', ' + str(windDir) + ', ' + str(windSpeed) + ', ' + str(shortForcast) + '\n'
+        for i in range(0, 13):
+            windDir = todayWeather[i].get('windDirection').replace(',', '-')
+            temp = todayWeather[i].get('temperature')
+            isDaytime = todayWeather[i].get('isDaytime')
+            detailedForecast = todayWeather[i].get('detailedForecast').replace(',', '') #ommit b/c has commas in them
+            shortForecast = todayWeather[i].get('shortForecast').replace(',', '')
+            windSpeed = todayWeather[i].get('windSpeed').replace(',', '')
+            startTime = todayWeather[i].get('startTime')
+            number = todayWeather[i].get('number')
+            endTime = todayWeather[i].get('endTime')
+            name = todayWeather[i].get('name').replace(',', '')
+            tempTrend = todayWeather[i].get('temperatureTrend')
+            #print(i) #use for testing
+            if i != 12:
+                outString += str(windDir) + ', ' + str(temp) + ', ' + str(isDaytime) + ', ' + str(detailedForecast) + ', ' + str(shortForecast) + ', ' + str(windSpeed) + ', ' + str(startTime) + ', ' + str(number) + ', ' + str(endTime) + ', ' + str(name) + ', ' + str(tempTrend) + ', ' #appending data in for loop until last line which need to add new line at end of data str
+            else: outString += str(windDir) + ', ' + str(temp) + ', ' + str(isDaytime) + ', ' + str(detailedForecast) + ', ' + str(shortForecast) + ', ' + str(windSpeed) + ', ' + str(startTime) + ', ' + str(number) + ', ' + str(endTime) + ', ' + str(name) + ', ' + str(tempTrend) + '\n' #last time in for loop add new line to data string
         cnt += 1
     except KeyboardInterrupt:
         raise   #this exception allow for ctrl^c to be pressed and program will end
     except: # ignore error that may occor when fethching data from noaa but report them to console
+        cnt += 1
         print('error:', sys.exc_info()[0])
         pass
-
-outString = header + outString 
+h = "windDirection, temperature[°F], isDaytime, detailedForecast, shortForecast, windSpeed, startTime, number, endTime, name, temperatureTrend" # use to attach 13 extra header info for weather of different times of day
+outString = header + h + ', ' + h + ', ' + h + ', ' + h + ', ' + h + ', ' + h + ', ' + h + ', ' + h + ', ' + h + ', ' + h + ', ' + h + ', ' + h + '\n' + outString #long line due to 12 titles 
 with open('weatherData.csv', 'w') as f: #write to json file to operate on
     f.write(str(outString))
     f.close()
